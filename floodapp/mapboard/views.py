1177856457430a6e34c8d2f52628b99962406048
Hlib Tereshchenko <23IMC10640@fh-krems.ac.at>
2024-11-27 23:34:54 +0100

    -

--- floodapp/mapboard/views.py
+++ floodapp/mapboard/views.py
@@ -3,22 +3,7 @@ from django.http import JsonResponse
 
 # Create your views here.from django.http import JsonResponse
 from .models import WaterLevel, Station
-import json
-# def water_levels_api(request):
-#     levels = WaterLevel.objects.select_related('region').all()
-#     data = [
-#         {
-#             "region": wl.region.name,
-#             "latitude": wl.region.latitude,
-#             "longitude": wl.region.longitude,
-#             "water_level": wl.water_level,
-#             "risk_level": wl.risk_level
-#         }
-#         for wl in levels
-#     ]
-#     return render(request, 'mapboard.html', {'data': data})
-
-
+import json 
 def mapboard_view(request):
     stations = Station.objects.all()
     data = [
