8bfbb2154e078c5419101380ab858b69d9a0e1f5
Denys <mashevskyydenisss@gmail.com>
2024-12-09 11:54:52 +0100

    frontend

--- floodapp/index/views.py
+++ floodapp/index/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
